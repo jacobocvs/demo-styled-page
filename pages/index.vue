c0c7cd73c89b9ba3ce5eed96f2c64156cac6dc31
Jacobo Cuevas <al.jacvs@gmail.com>
2024-11-18 18:39:26 -0600

    wip

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Something asd asd asd asd asd as das dasdsadas dasdasdas dasdasdas dadasd asd asdasdas dasd asdas dasdasdsa dasdasa sdsadadas dasdsd asd</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
