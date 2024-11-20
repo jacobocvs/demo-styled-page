5ce0bf3d67dd2bac938c52dd03b0ed1c6cb81815
Jacobo Cuevas <al.jacvs@gmail.com>
2024-11-20 13:15:42 -0600

    add: color palette

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <h1>Something asd asd asd asd asd as das dasdsadas dasdasdas dasdasdas dadasd asd asdasdas dasd asdas dasdasdsa dasdasa sdsadadas dasdsd asd</h1>
   </div>
 </template>
 
