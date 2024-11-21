9bdb23c901332a88737dd7e663965dc2aad87e22
jacobocvs <al.jacvs@gmail.com>
2024-11-20 20:17:53 -0600

    add: mesh gradient

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <section>
+      <slot/>
+      <div class="gradient_container">
+        <span class="box"></span>
+        <span class="box"></span>
+        <span class="box"></span>
+        <span class="box"></span>
+      </div>
+      <i class="fa-solid fa-arrow-pointer ef"></i>
+    </section>
+  </div>
+
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
+    <h1>Mesh Gradient Background Effect</h1>
+    <small>by <a href="https://github.com/vxncius-dev" target="_blank">@vxncius</a></small>
   </div>
 </template>
 
 <script setup lang="ts">
-
+definePageMeta({
+  layout: 'default'
+})
 </script>
 
 <style scoped>
