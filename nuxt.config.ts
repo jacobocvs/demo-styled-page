99b00b9c8ff3349cb8ece95a72fc43c27a20fdba
Jacobo Cuevas <al.jacvs@gmail.com>
2024-11-21 03:05:17 -0600

    add: Fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts
 export default defineNuxtConfig({
   ssr: false,
   compatibilityDate: '2024-04-03',
   devtools: {enabled: true},
+
   css: [
     '~/assets/scss/main.scss'
   ],
+
+  modules: [
+    ['@nuxtjs/google-fonts', {
+      families: {
+        'Noto+Sans': '200..800',
+        'Source+Code+Pro': '200..800'
+      },
+      display: 'swap',
+      prefetch: true,
+      preconnect: true,
+      download: true,
+      outputDir: 'assets',
+      stylePath: 'scss/base/_fonts.scss',
+      fontsPath: 'assets/fonts'
+    }]
+  ]
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <h1>Mesh Gradient Background Effect</h1>
-    <small>by <a href="https://github.com/vxncius-dev" target="_blank">@vxncius</a></small>
+    <h1>The browsing experience unlike any other</h1>
+  </div>
+  <div class="sparkle">
   </div>
 </template>
 
